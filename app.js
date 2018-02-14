06062f1da6a2226cee985e89473f04e54e48fdce
Alex Tov <atov@ucsd.edu>
2018-02-14 13:31:19 -0800

    attempted js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var recipeSearch = require('./routes/recipeSearch');
 // Example route
 // var user = require('./routes/user');
 
@@ -35,6 +36,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', index.view);
+app.get('/', recipeSearch.view);
 // Example route
 // app.get('/users', user.list);
 
