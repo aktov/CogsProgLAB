8016beac1d3af095292d083b83c226796445fdcc
Lillian Lee <lzl003@ucsd.edu>
2018-02-14 17:37:22 -0800

    Attempted to add counter, will continue later

---
+++ views/scripts.js
@@ -0,0 +1,9 @@
+
+var count = 0;
+
+function dishcount(){
+	var inputElems = new Boolean(document.getElementbyId('completed').checked);
+	if(inputElems==true){
+		alert("You've completed a recipe");	
+	}
+}
