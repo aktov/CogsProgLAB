ddc6229aa72f41adca197041afe3c33e11d122f7
Lillian Lee <lzl003@ucsd.edu>
2018-02-15 13:28:59 -0800

    changed icons, attempted counter

---
+++ public/js/scripts.js
@@ -0,0 +1,16 @@
+
+var count = 0;
+
+function dishcount(checkboxElem){
+	if(checkboxElem.checked){
+		count++;
+	}
+	else{
+		count--;
+	}
+	console.log(count);
+}
+
+function dishalert(){
+	alert("You've made"+ count + "dishes!");
+}
--- views/scripts.js
+++
@@ -1,9 +0,0 @@
-
-var count = 0;
-
-function dishcount(){
-	var inputElems = new Boolean(document.getElementbyId('completed').checked);
-	if(inputElems==true){
-		alert("You've completed a recipe");	
-	}
-}
