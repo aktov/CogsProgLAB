a80b9692696f8a0e29f00e9af0c9fce9c1569cc3
brn016 <brn016@ucsd.edu>
2018-02-22 15:21:24 -0800

    fixed recent page

--- routes/favorites.js
+++ routes/favorites.js
@@ -3,9 +3,9 @@
  * GET home page.
  */
 
-//var data = require('../favoriterecipes.json');
+var data = require('../favoriterecipes.json');
 
 exports.view = function(req, res){
 	//console.log(data);
-  res.render('favorites');
+  res.render('favorites', data);
 };
